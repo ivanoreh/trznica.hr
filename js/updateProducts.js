798cd65bfeca1a189292db74bae04f5951079428
ivanoreh <ivan.oreh@hotmail.com>
2016-07-08 17:19:17 +0200

    novo
    Signed-off-by: ivanoreh <ivan.oreh@hotmail.com>

--- js/updateProducts.js
+++ js/updateProducts.js
@@ -35,6 +35,62 @@ function updateProducts(){
         }
 
         lista.innerHTML = newInnerHTML;
-        $("#owl-example").owlCarousel();
+        $('.owl-carousel').owlCarousel();
+        $('.product-carousel').owlCarousel({
+            loop:true,
+            nav:true,
+            margin:20,
+            responsiveClass:true,
+            responsive:{
+                0:{
+                    items:1,
+                },
+                600:{
+                    items:3,
+                },
+                1000:{
+                    items:5,
+                }
+            }
+        });
+
+        $('.related-products-carousel').owlCarousel({
+            loop:true,
+            nav:true,
+            margin:20,
+            responsiveClass:true,
+            responsive:{
+                0:{
+                    items:1,
+                },
+                600:{
+                    items:2,
+                },
+                1000:{
+                    items:2,
+                },
+                1200:{
+                    items:3,
+                }
+            }
+        });
+
+        $('.brand-list').owlCarousel({
+            loop:true,
+            nav:true,
+            margin:20,
+            responsiveClass:true,
+            responsive:{
+                0:{
+                    items:1,
+                },
+                600:{
+                    items:3,
+                },
+                1000:{
+                    items:4,
+                }
+            }
+        });
     })
 }
