9133e02c829bf6c149d4fc30bc2594984e2a99b6
ivanoreh <ivan.oreh@hotmail.com>
2016-07-08 19:43:41 +0200

    novo
    Signed-off-by: ivanoreh <ivan.oreh@hotmail.com>

--- js/updateSellers.js
+++ js/updateSellers.js
@@ -18,8 +18,8 @@ function updateSellers(){
 
             var item = '<div class="single-wid-product">' +
                        '<a href="single-product.html"><img src="' + img + '" alt="" class="product-thumb"></a>' +
-                       '<h2><a href="single-product.html">' + resp[i].name.first + ' ' + resp[i].name.last + '</a></h2>' +
-                       '<h2><a href="single-product.html">Broj proizvoda: 26' +
+                        '<h2><a href="single-product.html">' + resp[i].name.first + ' ' + resp[i].name.last + '</a></h2>' +
+                       '<h2><a href="single-product.html">Broj proizvoda: ' + Math.round(Math.random() * 3)  +
                        '</div>';
 
             if (!resp[i].isAdmin)
