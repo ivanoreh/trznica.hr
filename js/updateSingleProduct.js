78dc3f8181a6518efe410fd9d8f4c79f74747bc5
ivanoreh <ivan.oreh@hotmail.com>
2016-07-08 18:28:01 +0200

    moje
    Signed-off-by: ivanoreh <ivan.oreh@hotmail.com>

--- js/updateSingleProduct.js
+++ js/updateSingleProduct.js
@@ -5,6 +5,7 @@
 function updateSingleProduct(id){
     XHR.get(url + 'product/' + id, function(item){
         console.log(item);
+        item = item[0];
 
         var img = "img/placeholder.png";
         try{
@@ -13,7 +14,7 @@ function updateSingleProduct(id){
 
         }
 
-        item = '<h2 class="product-name">Sony Smart TV - 2015</h2>' +
+        item = '<h2 class="product-name">'+ item["ime"] +'</h2>' +
             '<div class="product-inner-price">'+
             '<ins>' + item["cijena"] + '</ins>'+
             '</div>'+
@@ -30,9 +31,8 @@ function updateSingleProduct(id){
             '<div class="tab-content">'+
             '<div role="tabpanel" class="tab-pane fade in active" id="home">'+
             '<h2>Opis proizvoda/usluge</h2>'+
-            '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tristique, diam in consequat iaculis, est purus iaculis mauris, imperdiet facilisis ante ligula at nulla. Quisque volutpat nulla risus, id maximus ex aliquet ut. Suspendisse potenti. Nulla varius lectus id turpis dignissim porta. Quisque magna arcu, blandit quis felis vehicula, feugiat gravida diam. Nullam nec turpis ligula. Aliquam quis blandit elit, ac sodales nisl. Aliquam eget dolor eget elit malesuada aliquet. In varius lorem lorem, semper bibendum lectus lobortis ac.</p>'+
+            '<p>'+ item["detalji"]+ '</p>'+
 
-            '<p>Mauris placerat vitae lorem gravida viverra. Mauris in fringilla ex. Nulla facilisi. Etiam scelerisque tincidunt quam facilisis lobortis. In malesuada pulvinar neque a consectetur. Nunc aliquam gravida purus, non malesuada sem accumsan in. Morbi vel sodales libero.</p>'+
             '</div>'+
             '<div role="tabpanel" class="tab-pane fade" id="profile">'+
             '<h2>Lokacija</h2>'+
@@ -42,5 +42,6 @@ function updateSingleProduct(id){
             '</div>';
 
         document.getElementById("proizvod").innerHTML = item;
+        document.getElementById("proizvod-img").src = img;
     })
 }
