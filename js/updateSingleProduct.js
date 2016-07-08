f2b5048fbf3d8495e5ee3f850feaab53630b5146
ivanoreh <ivan.oreh@hotmail.com>
2016-07-08 18:55:00 +0200

    novo
    Signed-off-by: ivanoreh <ivan.oreh@hotmail.com>

--- js/updateSingleProduct.js
+++ js/updateSingleProduct.js
@@ -7,6 +7,12 @@ function updateSingleProduct(id){
         console.log(item);
         item = item[0];
 
+        XHR.get(url + 'category/' + item["kategorija"][0], function(resp){
+            console.log(resp);
+            document.getElementById("category").innerHTML = "<a>" + resp[0].ime + "</a>";
+            document.getElementById("kategorija-item").innerHTML = "<a>" + resp[0].ime + "</a>";
+        });
+
         var img = "img/placeholder.png";
         try{
             img = item.slike[0].url;
@@ -16,11 +22,11 @@ function updateSingleProduct(id){
 
         item = '<h2 class="product-name">'+ item["ime"] +'</h2>' +
             '<div class="product-inner-price">'+
-            '<ins>' + item["cijena"] + '</ins>'+
+            '<ins>' + item["cijena"] + ' Kn</ins>'+
             '</div>'+
 
             '<div class="product-inner-category">'+
-            '<p>Kategorija: <a href="" id="kategorija-item">Summer</a>. </p>'+
+            '<p>Kategorija: <a href="" id="kategorija-item">' +  + '</a>. </p>'+
             '</div>'+
 
             '<div role="tabpanel">'+
@@ -35,7 +41,7 @@ function updateSingleProduct(id){
 
             '</div>'+
             '<div role="tabpanel" class="tab-pane fade" id="profile">'+
-            '<h2>Lokacija</h2>'+
+            '<h2 id="lokacija">Lokacija</h2>'+
             '<p>A2</p>'+
             '</div>'+
             '</div>'+
